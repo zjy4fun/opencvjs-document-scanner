192cd6853d60d6ceebaa6c799623e2926f013c8d
xulihang <xulihanghai@163.com>
2025-09-11 16:02:30 +0800

    remove the vite sample

--- example/vite/src/main.ts
+++
@@ -1,8 +0,0 @@
-
-import { DDV } from 'dynamsoft-document-viewer';
-import { DocumentScanner, setDDV, OpenCVDocumentDetectHandler } from 'opencv-document-scanner';
-
-(window as any)["DocumentScanner"] = DocumentScanner;
-(window as any)["Dynamsoft"] = {"DDV":DDV};
-(window as any)["OpenCVDocumentDetectHandler"] = OpenCVDocumentDetectHandler;
-setDDV(DDV);
